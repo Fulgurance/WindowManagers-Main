f09e6903e00dcccc68c0eb5ffd86a108d254cb92
fulgurance <zohran.londais@gmail.com>
2023-04-07 23:10:59 +0100

    Added i3 installer

---
+++ I3/4.22.0/4.22.0.cr
@@ -0,0 +1,28 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runMesonCommand([   "--prefix=/usr",
+                            ".."],
+                            buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+    
+    def prepareInstallation
+        super
+
+        runNinjaCommand(["install"],buildDirectoryPath,{"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+end
